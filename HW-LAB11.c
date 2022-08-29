3b7daa368cdc5b1d061e91a2cb301ba9131c42c0
Meryem Azreg <80335241+MeryemAzreg@users.noreply.github.com>
2022-08-29 13:30:37 +0300

    Add files via upload

---
+++ HW-LAB11.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	//Pentagon numbers are produced according to the formula Pn=n(3n-1)/2. Print pentagon numbers less than 100 to the screen.
+	
+	
+//	int res=0,c=1;
+//	while(res<100){
+//		res=c*(3*c-1)/2;
+//		if(res>100)
+//		   break;
+//		printf("the pent nums : %d\n",res);
+//		c++;   
+//	}
+
+
+   	//Write the C code that adds two matrices with the same number of rows and columns. Matrix values will be entered from the keyboard.
+   	
+   	
+//   	int x,y,i,j;
+//   	printf("enter the rows :");
+//   	scanf("%d",&x);
+//   	printf("enter the colms :");
+//   	scanf("%d",&y);
+//   	
+//   	int a[x][y],b[x][y],c[x][y];
+//   	
+//   	for(i=0;i<x;i++){
+//   		for(j=0;j<y;j++){
+//   			printf("enter the a[%d][%d] > ",i,j);
+//   			scanf("%d",&a[i][j]);
+//		   }
+//	   }
+//	for(i=0;i<x;i++){
+//   		for(j=0;j<y;j++){
+//   			printf("enter the b[%d][%d] > ",i,j);
+//   			scanf("%d",&b[i][j]);
+//		   }
+//	   }
+//   	for(i=0;i<x;i++){
+//   		for(j=0;j<y;j++){
+//   			c[i][j]=a[i][j]+b[i][j];
+//   			printf("c[%d][%d] is %d\n",i,j,c[i][j]);
+//		   }
+//	   }
+
+
+   	//Write a program in C language that transfers the height and weight values of 10 students in a class to a two-dimensional array by reading them from the keyboard and calculates the 
+	//average height and weight values and prints them on the screen.
+	
+	
+//	int a[10][2],w,h,avgh=0,avgw=0,i,j;
+//	
+//	for(i=0;i<10;i++){
+//		for(j=0;j<2;j++){
+//			if(j==0){
+//				printf("entr height a[%d]: ",i);
+//				scanf("%d",&a[i][0]);
+//				avgh=avgh+a[i][j];
+//			}
+//			else{
+//				printf("entr weight a[%d]: ",i);
+//				scanf("%d",&a[i][1]);
+//				avgw=avgw+a[i][j];
+//			}
+//		}
+//	}
+//	avgh/=10;
+//	avgw/=10;	
+//	
+//	printf("avg height is: %d \navg weight is: %d", avgh, avgw);
+
+
+	
+	return 0;
+}
